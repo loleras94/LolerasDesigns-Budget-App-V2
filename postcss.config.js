aefea8dd9e111c7dad95c9014923d9eaca1cb3f8
loleras94 <alexandris.alex@hotmail.com>
2025-10-29 14:34:58 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+// postcss.config.js
+export default {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 };
